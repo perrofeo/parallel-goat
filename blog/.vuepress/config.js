e39ef5def665ab8e54667c9527ed966955569b07
igor <perrofeofilms@gmail.com>
2019-11-10 15:52:22 -0600

    -

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,8 +1,6 @@
-module.exports = {
-  title: 'Theme Title',
-  description: 'Theme description',
-  base: '/',
-  theme: 'casper',
+const config = require('./data/config.json');
+
+module.exports = Object.assign(config, {
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }]
   ],
@@ -11,44 +9,5 @@ module.exports = {
       permalink: false,
       permalinkBefore: false
     }
-  },
-  themeConfig: {
-    cover: '/images/cover.jpg',
-    logo: '/images/logo.png',
-    nav: [{
-      text: 'Home',
-      link: '/'
-    }, {
-      text: 'Posts',
-      link: '/posts'
-    }, {
-      text: 'Category',
-      link: '/category/some-category'
-    }, {
-      text: 'Page',
-      link: '/a-page.html'
-    }],
-
-    footer: [{
-      text: 'Latest Posts',
-      link: '/posts'
-    }, {
-      text: 'Facebook',
-      link: 'https://facebook.com/'
-    }, {
-      text: 'Twitter',
-      link: 'https://twitter.com'
-    }, {
-      text: 'Github',
-      link: 'https://github.com/'
-    }],
-    social: {
-      github: 'https://github.com',
-      twitter: 'https://twitter.com',
-      facebook: 'https://facebook.com',
-      xing: 'https://xing.de',
-      instagram: 'https://instagram.com',
-      linkedin: 'https://linkedin.com'
-    }
   }
-}
+});
